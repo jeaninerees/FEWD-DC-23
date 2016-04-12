c762467c25a3eb8618f2dd0e13ed2401b4a84292
jeaninerees <jeanine.rees@gmail.com>
2016-04-12 18:47:14 -0400

    Changes

--- Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
+++ Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,28 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function() {
+
+
+//Implement the red light using jQuery. Don't forget to addto the script tags.
+
+$("#stopButton").click(colorStopLight);
+$("#slowButton").click(colorSlowLight);
+$("#goButton").click(colorGoLight);
+
+
+
+function colorStopLight() {
+	$("#stopLight").css("backgroundColor", "red");
+	$("#slowLight, #goLight").css("backgroundColor", "black");
+}
+
+function colorSlowLight() {
+	$("#slowLight").css("backgroundColor", "yellow");
+	$("#stopLight, #goLight").css("backgroundColor", "black");
+}
+
+function colorGoLight() {
+	$("#goLight").css("backgroundColor", "green");
+	$("#slowLight, #stopLight").css("backgroundColor", "black");
+}
+
+
+});
